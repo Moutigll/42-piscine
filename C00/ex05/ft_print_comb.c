47972de46b7377ca5df0cb9b76c4015510971a31
Ewen Le Leannec <ele-lean@c1r6p3.42lehavre.fr>
2024-08-08 11:14:58 +0200

    ex05

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -1,27 +1,48 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ele-lean <ele-lean@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/07 20:06:37 by ele-lean          #+#    #+#             */
-/*   Updated: 2024/08/07 20:25:54 by ele-lean         ###   ########.fr       */
+/*   Created: 2024/08/07 20:26:57 by ele-lean          #+#    #+#             */
+/*   Updated: 2024/08/08 11:13:32 by ele-lean         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-int	ft_is_negative(int n)
+void	ft_putchar(char c)
 {
-	if (n >= 0)
+	write(1, &c, 1);
+}
+
+void	ft_print_comb(void)
+{
+	int	a[3];
+
+	a[0] = '0';
+	while (a[0] <= '7')
 	{
-		putchar('P');
+		a[1] = a[0] + 1;
+		while (a[1] <= '8')
+		{
+			a[2] = a[1] + 1;
+			while (a[2] <= '9')
+			{
+				ft_putchar(a[0]);
+				ft_putchar(a[1]);
+				ft_putchar(a[2]);
+				if (a[0] != '7')
+				{
+					ft_putchar(',');
+					ft_putchar(' ');
+				}
+				a[2]++;
+			}
+			a[1]++;
+		}
+		a[0]++;
 	}
-	else
-	{
-		putchar('N');
-	}
-	return (0);
 }
