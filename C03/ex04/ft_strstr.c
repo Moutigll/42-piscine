0bbad5f72524b5ee1c7b291dca1aa6e23fb7cdce
Ewen Le Leannec <ele-lean@c1r6p2.42lehavre.fr>
2024-08-12 14:14:56 +0200

    C03

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -6,11 +6,51 @@
 /*   By: ele-lean <ele-lean@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 10:56:53 by ele-lean          #+#    #+#             */
-/*   Updated: 2024/08/12 10:57:14 by ele-lean         ###   ########.fr       */
+/*   Updated: 2024/08/12 12:29:06 by ele-lean         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 char	*ft_strstr(char *str, char *to_find)
 {
-	
+	int	i;
+	int	j;
+
+	if (*to_find == '\0')
+		return ((char *)str);
+	if (str == NULL || to_find == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == to_find[0])
+		{
+			j = 1;
+			while (str[i + j] == to_find[j] && to_find[j] != '\0')
+			{
+				j++;
+			}
+			if (to_find[j] == '\0')
+			{
+				return ((str + i));
+			}
+		}
+		i++;
+	}
+	return (NULL);
 }
+
+/*int	main(void)
+{
+	char	a[] = "abcdefghijklmnopqrstuvwxyz";
+	char	b[] = "defgh";
+	char	c[] = "pruegh";
+	char	d[] = "";
+	char	e[] = "abcd";
+
+	printf("\n%s", ft_strstr(a, b));
+	printf("\n%s", ft_strstr(a, c));
+	printf("\n%s", ft_strstr(a, d));
+	printf("\n%s", ft_strstr(a, e));
+}*/
--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -1,40 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ele-lean <ele-lean@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/12 10:28:04 by ele-lean          #+#    #+#             */
-/*   Updated: 2024/08/12 12:39:35 by ele-lean         ###   ########.fr       */
+/*   Created: 2024/08/12 12:34:45 by ele-lean          #+#    #+#             */
+/*   Updated: 2024/08/12 14:14:18 by ele-lean         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-char	*ft_strncat(char *dest, char *src, unsigned int nb)
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	int				i;
+	unsigned int	i;
 	unsigned int	j;
 
 	i = 0;
 	while (dest[i] != '\0')
 		i++;
 	j = 0;
-	while (src[j] != '\0' && j < nb)
+	while (src[j] != '\0' && i + j < size)
 	{
 		dest[i + j] = src[j];
 		j++;
 	}
 	dest[i + j] = '\0';
-	return (dest);
+	j = 0;
+	while (src[j] != '\0')
+		j++;
+	return (i + j);
 }
 
-/*int main(void)
+int main(void)
 {
 	char a[] = "abcde";
 	char b[] = "fghij";
 
-	ft_strncat(a, b, 3);
+	ft_strlcat(a, b, 10);
 	printf("%s", a);
-}*/
+}
