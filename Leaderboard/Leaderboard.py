42aab78daad06fb8e812d2373f320ada36dd303d
Moutig <moutig-tan@proton.me>
2024-08-21 04:31:53 +0200

    intra leaderboard v1

---
+++ Leaderboard/Leaderboard.py
@@ -0,0 +1,121 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Leaderboard-skill.py                               :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: ele-lean <ele-lean@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/08/20 16:58:45 by ele-lean          #+#    #+#              #
+#    Updated: 2024/08/20 19:29:24 by ele-lean         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+try:
+	import os
+	import sys
+	import subprocess
+	import json
+	import dropbox
+	from datetime import datetime
+
+	ACCESS_TOKEN = 'sl.B7UOXlw1SNObS9GswO4PvV3Ze9H8NJq0DGCrcxx9fjLB1qSa8qe2IARaQ8EKjgadL7m2buyn-PNLOjtYSPiSn4r2SozVYgYt08C7YtHmWAM3wWkuvAU0vjT5wpCBLDbC5Rj33wwkplrku_4sS2-A6Kc'
+	dbx = dropbox.Dropbox(ACCESS_TOKEN)
+	dropbox_file_path = '/scores.json'
+
+	_, res = dbx.files_download(dropbox_file_path)
+	scores_liste = json.loads(res.content.decode('utf-8'))
+
+	a = 0
+	min_index = 0
+	def col(i):
+		if i < 11:
+			return "\033[0;95m"
+		elif i < 51:
+			return "\033[0;92m"
+		elif i < 76:
+			return "\033[0;96m"
+		else:
+			return "\033[0;91m"
+
+	for i in range(len(scores_liste)):
+		a = i
+		min_index = i
+		while a < len(scores_liste):
+			if scores_liste[min_index][1] < scores_liste[a][1]:
+				min_index = a
+			a+=1
+		if min_index != i:
+				temp = scores_liste[i]
+				scores_liste[i] = scores_liste[min_index]
+				scores_liste[min_index] = temp
+	print("""\033[0;93m
+                                   ___---___
+                             ___---___---___---___
+                       ___---___---    *    ---___---___
+                 ___---___---    _   _    ___    ---___---___
+           ___---___---         | | | |  (_  |         ---___---___
+     ___---___---               `.__  |   / /_               ---___---___
+__---___---_________________________|_|__|____|____________________---___---__
+==============================================================================
+ ||||           Leaderboard for the july piscine of 42 Le Havre           ||||
+ |---------------------------------------------------------------------------|
+ |___-----___-----------------------------------------------------___-----___|
+ / _ \===/ _ \                                                   / _ \===/ _ \\
+ \__/=====\__/                     __________                    \__/=====\__/
+    |||||||                        |\033[0;33m""" + (scores_liste[0][0] + " " * (8 - len(scores_liste[0][0]))) + """\033[0;93m|                       |||||||
+    |||||||        \033[0;90m__________\033[0;93m      |   🥇   |\033[0;93m                       |||||||
+    |||||||        \033[0;90m|""" + (scores_liste[1][0] + " " * (8 - len(scores_liste[1][0]))) + """|\033[0;93m      \  """ + str(scores_liste[0][1]) + """  /\033[0;93m                       |||||||
+    |||||||        \033[0;90m|   🥈   |\033[0;93m       |      |\033[0;93m       \033[0;94m__________ \033[0;93m      |||||||
+    |||||||        \033[0;90m\  """ + str(scores_liste[1][1]) + """  /       \033[0;93m|      |\033[0;93m       \033[0;94m|""" + scores_liste[2][0] + """|\033[0;93m       |||||||
+    |||||||         \033[0;90m|      |\033[0;93m        |      |\033[0;93m       \033[0;94m|   🥉   |\033[0;93m       |||||||
+    |||||||         \033[0;90m|      |\033[0;93m        |      |\033[0;93m       \033[0;94m|  """ + str(scores_liste[2][1]) + """  |\033[0;93m       |||||||
+    |||||||         \033[0;90m|000110|\033[0;93m        |101010|\033[0;93m       \033[0;94m\ 001001 /\033[0;93m       |||||||
+    (oOoOo)         \033[0;90m(oOooOo)\033[0;93m        (oOooOo)\033[0;93m       \033[0;94m (oOooOo)\033[0;93m        (oOoOo)
+    J%%%%%L         \033[0;90mJ%%%%%%L\033[0;93m        J%%%%%%L\033[0;93m       \033[0;94m J%%%%%%L\033[0;93m        J%%%%%L
+   ZZZZZZZZZ       \033[0;90mZZZZZZZZZZ\033[0;93m      ZZZZZZZZZZ\033[0;93m      \033[0;94mZZZZZZZZZZ\033[0;93m      ZZZZZZZZZ
+===============================================================================
+  ===========================================================================
+  |___-----___-----___-----___---------------------___-----___-----___-----___|
+  / _ \===/ _ \   / _ \===/ _ \                   / _ \===/ _ \   / _ \===/ _ \\
+  \__/=====\__/   \__/=====\__/                   \__/=====\__/   \__/=====\__/""")
+	for i  in range(3, len(scores_liste)):
+		print("""\033[0;93m     |||||||         |||||||    """ + col(i) + (str((i)) + ":" + " " * (2 - len(str(i)))) + " " + (scores_liste[i][0] + " " * (8 - len(scores_liste[i][0]))) + "  " + str(scores_liste[i][1]) + (" " * (7 - len(str(scores_liste[i][1])))) + """\033[0;93m|||||||         |||||||""")
+	
+	metadata = dbx.files_get_metadata(dropbox_file_path)
+	modification_time = metadata.client_modified
+	print("""\033[0;93m     |||||||         |||||||     \033[0;34mLast data update\033[0;93m    |||||||         |||||||""")
+	print("""\033[0;93m     |||||||         |||||||   \033[0;34m""" + str(modification_time) + """\033[0;93m   |||||||         |||||||""")
+
+
+	print(""" ===============================================================================
+   |_________________________________________________________________________|
+  |___________________________________________________________________________|
+ |_____________________________________________________________________________|\033[0;0m""")
+except Exception as e:
+	print("\033[0;91m")
+	exc_type, exc_obj, exc_tb = sys.exc_info()
+	fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+	print(exc_type, fname, exc_tb.tb_lineno)
+	a = input("Multiple modules are required to run this script do you wanna try to automatically install them ?")
+	def install(package):
+		subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+	required_modules = [
+		"dropbox",
+		"json",
+		"selenium",
+		"monotonic",
+		"getpass",
+		"time",
+		"pathlib",
+		"datetime"
+	]
+
+	for module in required_modules:
+		try:
+			__import__(module)
+		except ImportError:
+			print(f"Module {module} not found, installing...")
+			install(module)
+		else:
+			print(f"Module {module} already installed.")
+	print("\033[0;0m")
--- Leaderboard/scrap.py
+++ Leaderboard/scrap.py
@@ -10,105 +10,152 @@
 #                                                                              #
 # **************************************************************************** #
 
-import json
-import time
-import os
-import monotonic
-import getpass
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-prenoms = [
-    "ele-lean", "malevieu", "lchauvet", "almeddah", "cpoulain", "jcheron", "tobesnar",
-    "ladurand", "edetoh", "releroy", "vseyidov", "tle-goff", "throbert", "lnassar",
-    "cabraham", "mduvey", "ghoyaux", "aberenge", "lsellier", "ljacq", "abidaux",
-    "cfleuret", "adcisse", "tgoudman", "hdelacou", "hdruel", "nlecreux", "jabertin",
-    "huyanan", "mavander", "lvan-bre", "niclambe", "jbastard", "aperceva", "abosc",
-    "tolivaud", "calleaum", "emaillet", "rbardet-", "lgrisel", "alegrix", "tharrel",
-    "aboutry", "ameduboi", "mgendrot", "tliberse", "mpelleri", "ngaudoui", "etaquet",
-    "dbaciu", "mpapin", "minhpham", "fkurti", "jeetienn", "mgarsaul", "guphilip",
-    "afolliot", "plerick", "clephal", "pilot-de", "sbenamar", "artgirar", "tlize",
-    "maiba", "pthuilli", "ogsimsek", "lleborgn", "luguo", "azhao", "fbriault",
-    "mtatar", "salwong", "rdelepin", "mkreko", "lplottin", "isehim", "bperrot",
-    "spicque", "abensala", "jtannier", "jhatting", "matgabri", "gdefrace", "abuty",
-    "ngilles", "jnicolle", "qgutmann", "davlelie", "taubry", "mflandri", "rbouchou",
-    "ansleima", "jgrisoni", "hzwak", "apain", "jliot"
-]
-
-
-print("Nombre d'élèves: " + str(len(prenoms)))
-
-login = input("Login: ")
-mdp = getpass.getpass('Mdp: ')
-dl = int(input("Temps entre chaques requetes: "))
-driver = webdriver.Chrome()
-scores = []
-scores_liste = []
-temps_presence_liste = []
-
-a = monotonic.monotonic()
 try:
-    driver.get("https://auth.42.fr/auth/realms/students-42/protocol/openid-connect/auth?client_id=intra")
+    import json
+    import time
+    import os
+    import sys
+    import subprocess
+    import monotonic
+    import getpass
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+    import dropbox
+    from pathlib import Path
 
-    username = driver.find_element(By.ID, "username")
-    password = driver.find_element(By.ID, "password")
+    DROPBOX_ACCESS_TOKEN = "sl.B7UOXlw1SNObS9GswO4PvV3Ze9H8NJq0DGCrcxx9fjLB1qSa8qe2IARaQ8EKjgadL7m2buyn-PNLOjtYSPiSn4r2SozVYgYt08C7YtHmWAM3wWkuvAU0vjT5wpCBLDbC5Rj33wwkplrku_4sS2-A6Kc"
 
-    username.send_keys(login)
-    password.send_keys(mdp)
-    
-    submit_button = driver.find_element(By.ID, "kc-login")
-    submit_button.click()
-    WebDriverWait(driver, 10).until(
-        EC.url_contains("https://profile.intra.42.fr")
-    )
+    dbx = dropbox.Dropbox(DROPBOX_ACCESS_TOKEN)
 
-    for index, prenom in enumerate(prenoms):
-        driver.get(f"https://profile.intra.42.fr/users/{prenom}/skills")
+    prenoms = [
+        "ele-lean", "malevieu", "lchauvet", "almeddah", "cpoulain", "jcheron", "tobesnar",
+        "ladurand", "edetoh", "releroy", "vseyidov", "tle-goff", "throbert", "lnassar",
+        "cabraham", "mduvey", "ghoyaux", "aberenge", "lsellier", "ljacq", "abidaux",
+        "cfleuret", "adcisse", "tgoudman", "hdelacou", "hdruel", "nlecreux", "jabertin",
+        "huyanan", "mavander", "lvan-bre", "niclambe", "jbastard", "aperceva", "abosc",
+        "tolivaud", "calleaum", "emaillet", "rbardet-", "lgrisel", "alegrix", "tharrel",
+        "aboutry", "ameduboi", "mgendrot", "tliberse", "mpelleri", "ngaudoui", "etaquet",
+        "dbaciu", "mpapin", "minhpham", "fkurti", "jeetienn", "mgarsaul", "guphilip",
+        "afolliot", "plerick", "clephal", "pilot-de", "sbenamar", "artgirar", "tlize",
+        "maiba", "pthuilli", "ogsimsek", "lleborgn", "luguo", "azhao", "fbriault",
+        "mtatar", "salwong", "rdelepin", "mkreko", "lplottin", "isehim", "bperrot",
+        "spicque", "abensala", "jtannier", "jhatting", "matgabri", "gdefrace", "abuty",
+        "ngilles", "jnicolle", "qgutmann", "davlelie", "taubry", "mflandri", "rbouchou",
+        "ansleima", "jgrisoni", "hzwak", "apain", "jliot"
+    ]
 
+    print("\033[0;91m!!!WARNING!!! READ THIS BEFORE CONTINUING !!!WARNING!!!\nThis script will ask you for your login, it will be only used to get access to the data.\nThe chances of being flagg by the server are low but you can add a custom delay between each request to slow down the traffic.\nA google chrome page will automatically be opened for the duration of the operation (around 30-35sec with no delay) don't close it, it could mess up the operation.\033[0;0m")
+    print("Number of students: " + str(len(prenoms)))
+
+    login = input("Login: ")
+    mdp = getpass.getpass('Mdp: ')
+    dl = int(input("Delay between each request: "))
+    driver = webdriver.Chrome()
+    scores = []
+    scores_liste = []
+    temps_presence_liste = []
+    a = monotonic.monotonic()
+    try:
+        driver.get("https://auth.42.fr/auth/realms/students-42/protocol/openid-connect/auth?client_id=intra")
+
+        username = driver.find_element(By.ID, "username")
+        password = driver.find_element(By.ID, "password")
+
+        username.send_keys(login)
+        password.send_keys(mdp)
+        
+        submit_button = driver.find_element(By.ID, "kc-login")
+        submit_button.click()
         WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.TAG_NAME, "pre"))
+            EC.url_contains("https://profile.intra.42.fr")
         )
-        page_content = driver.find_element(By.TAG_NAME, "pre").text
-        try:
-            skills = json.loads(page_content)
-        except json.JSONDecodeError:
-            print(f"Erreur de décodage JSON pour {prenom}")
-            scores.append(0)
-            continue
-        total_percent = sum(skill.get("percent", 0) for skill in skills)
-        total_level = sum(skill.get("lvl", 0) * 100 for skill in skills)
-        score = total_percent + total_level
-        scores.append(score)
-        scores_liste.append([prenom, score])
 
-        time.sleep(dl)
-        driver.get(f"https://translate.intra.42.fr/users/{prenom}/locations_stats.json")
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.TAG_NAME, "pre"))
-        )
-        presence_content = driver.find_element(By.TAG_NAME, "pre").text
-        try:
-            presence_stats = json.loads(presence_content)
-            temps_presence = [[jour, temps] for jour, temps in presence_stats.items()]
-            temps_presence_liste.append([prenom, temps_presence])
-        except json.JSONDecodeError:
-            print(f"Erreur de décodage JSON pour les temps de présence de {prenom}")
-            temps_presence_liste.append([prenom, []])
+        for index, prenom in enumerate(prenoms):
+            driver.get(f"https://profile.intra.42.fr/users/{prenom}/skills")
 
-        print(f"Score pour {prenom}: {score}")
-        print(f"Temps de présence pour {prenom}: {temps_presence}")
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.TAG_NAME, "pre"))
+            )
+            page_content = driver.find_element(By.TAG_NAME, "pre").text
+            try:
+                skills = json.loads(page_content)
+            except json.JSONDecodeError:
+                print(f"Erreur de décodage JSON pour {prenom}")
+                scores.append(0)
+                continue
+            total_percent = sum(skill.get("percent", 0) for skill in skills)
+            total_level = sum(skill.get("lvl", 0) * 100 for skill in skills)
+            score = total_percent + total_level
+            scores.append(score)
+            scores_liste.append([prenom, score])
 
-        time.sleep(dl)
+            time.sleep(dl)
+            driver.get(f"https://translate.intra.42.fr/users/{prenom}/locations_stats.json")
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.TAG_NAME, "pre"))
+            )
+            presence_content = driver.find_element(By.TAG_NAME, "pre").text
+            try:
+                presence_stats = json.loads(presence_content)
+                temps_presence = [[jour, temps] for jour, temps in presence_stats.items()]
+                temps_presence_liste.append([prenom, temps_presence])
+            except json.JSONDecodeError:
+                print(f"Erreur de décodage JSON pour les temps de présence de {prenom}")
+                temps_presence_liste.append([prenom, []])
 
-finally:
-    driver.quit()
-    print("Duree de l'operation: " + str(monotonic.monotonic() - a))
+            print(f"Score pour {prenom}: {score}")
+            print(f"Temps de présence pour {prenom}: {temps_presence}")
 
-with open("scores.json", "w") as fichier:
-    json.dump(scores_liste, fichier, indent=4)
-with open("temps_presence.json", "w") as fichier:
-    json.dump(temps_presence_liste, fichier, indent=4)
+            time.sleep(dl)
 
-print("Scores reçus: " + str(len(scores)))
+    finally:
+        driver.quit()
+        print("Duration of scrapping: " + str(monotonic.monotonic() - a))
+
+    documents_folder = Path.home() / "Documents"
+    documents_folder.mkdir(parents=True, exist_ok=True)
+    scores_file = documents_folder / "scores.json"
+    presence_file = documents_folder / "temps_presence.json"
+
+    with open(scores_file, "w") as fichier:
+        json.dump(scores_liste, fichier, indent=4)
+    with open(presence_file, "w") as fichier:
+        json.dump(temps_presence_liste, fichier, indent=4)
+
+    def upload_to_dropbox(file_path, dropbox_path):
+        with open(file_path, "rb") as f:
+            dbx.files_upload(f.read(), dropbox_path, mode=dropbox.files.WriteMode.overwrite)
+            print(f"File {file_path} uploaded on Dropbox at {dropbox_path}")
+
+    upload_to_dropbox(str(scores_file), f"/{scores_file.name}")
+    upload_to_dropbox(str(presence_file), f"/{presence_file.name}")
+
+    print("Scores recieved: " + str(len(scores)))
+except Exception as e:
+	print("\033[0;91m")
+	exc_type, exc_obj, exc_tb = sys.exc_info()
+	fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+	print(exc_type, fname, exc_tb.tb_lineno)
+	a = input("Multiple modules are required to run this script do you wanna try to automatically install them ?")
+	def install(package):
+		subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+	required_modules = [
+		"dropbox",
+		"json",
+		"selenium",
+		"monotonic",
+		"getpass",
+		"time",
+		"pathlib"
+	]
+	for module in required_modules:
+		try:
+			__import__(module)
+		except ImportError:
+			print(f"Module {module} non trouvé, installation...")
+			install(module)
+		else:
+			print(f"Module {module} déjà installé.")
+	print("if it's still not working you can try 'sudo apt-get install chromium-chromedriver'\033[0;0m")
