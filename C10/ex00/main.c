ac336b14bb8c00d5f7f8c211c3ca1c64c6cacca4
Moutig <99372574+Moutigll@users.noreply.github.com>
2024-08-28 13:49:33 +0200

    Progression on C10

--- C10/ex00/main.c
+++ C10/ex00/main.c
@@ -6,7 +6,7 @@
 /*   By: ele-lean <ele-lean@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 15:05:22 by ele-lean          #+#    #+#             */
-/*   Updated: 2024/08/27 16:27:47 by ele-lean         ###   ########.fr       */
+/*   Updated: 2024/08/28 11:28:25 by ele-lean         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,51 +18,57 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-int	ft_strlen(char *str)
-{
-	int		i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
 void	ft_putstr(char *str, int len)
 {
 	int		i;
 
-	if (len == 0)
-		len = ft_strlen(str);
-	i = -1;
-	while (++i < len)
+	i = 0;
+	while (i < len)
+	{
 		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	check_arg(int argc)
+{
+	if (argc < 2)
+	{
+		write(1, "File name missing.\n", 19);
+		return (1);
+	}
+	if (argc > 2)
+	{
+		write(1, "Too many arguments.\n", 20);
+		return (1);
+	}
+	return (0);
 }
 
 int	main(int argc, char **argv)
 {
 	int		fd;
-	char	buf[4097];
 	int		size;
-	int		i;
+	char	buf[4097];
 
-	if (argc <= 1)
+	if (check_arg(argc) == 0)
 	{
-		write(1, "File name missing.", 18);
-		return (0);
+		fd = open(argv[1], O_RDONLY);
+		if (fd == -1)
+		{
+			write(1, "Cannot read file.\n", 18);
+			return (1);
+		}
+		size = read(fd, buf, 4096);
+		while (size > 0)
+		{
+			buf[size] = '\0';
+			ft_putstr(buf, size);
+			size = read(fd, buf, 4096);
+		}
+		if (size == -1)
+			write(1, "Error reading file.\n", 20);
+		close(fd);
 	}
-	if (argc > 2)
-	{
-		write(1, "Too many arguments.", 19);
-		return (0);
-	}
-	fd = open(argv[1], O_RDONLY);
-	size = read(fd, buf, 4096);
-	i = -1;
-	if (fd != -1)
-		while (buf[i++])
-			ft_putchar(buf[i]);
-	else
-		write(1, "Cannot read file.", 17);
-	close(fd);
+	return (0);
 }
---
+++ C10/ex01/ft_cat.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_cat.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ele-lean <ele-lean@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/28 12:28:36 by ele-lean          #+#    #+#             */
+/*   Updated: 2024/08/28 13:14:02 by ele-lean         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+#define BUF_SIZE 32768
+
+void	print_error(const char *msg, const char *file)
+{
+	if (file != NULL)
+	{
+		write(2, file, strlen(file));
+		write(2, ": ", 2);
+	}
+	write(2, msg, strlen(msg));
+	write(2, "\n", 1);
+}
+
+void	ft_cat(int fd)
+{
+	char	buf[BUF_SIZE];
+	ssize_t	bytes_read;
+	ssize_t	result;
+
+	bytes_read = read(fd, buf, BUF_SIZE);
+	while (bytes_read > 0)
+	{
+		result = write(1, buf, bytes_read);
+		if (result == -1)
+		{
+			print_error(strerror(errno), NULL);
+			close(fd);
+			return ;
+		}
+		bytes_read = read(fd, buf, BUF_SIZE);
+	}
+	if (bytes_read == -1)
+		print_error(strerror(errno), NULL);
+	close(fd);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	fd;
+	int	index;
+
+	index = 1;
+	if (argc == 1)
+		ft_cat(0);
+	else
+	{
+		while (index < argc)
+		{
+			fd = open(argv[index], O_RDONLY);
+			if (fd == -1)
+				print_error(strerror(errno), argv[index]);
+			else
+				ft_cat(fd);
+			index++;
+		}
+	}
+	return (0);
+}
