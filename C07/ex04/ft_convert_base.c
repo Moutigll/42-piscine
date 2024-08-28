2c17e65929aafe1740c840e139b040b0a59dbefe
Moutig <99372574+Moutigll@users.noreply.github.com>
2024-08-28 21:14:09 +0200

    Corrected C07

--- C07/ex04/ft_convert_base.c
+++ C07/ex04/ft_convert_base.c
@@ -6,12 +6,15 @@
 /*   By: ele-lean <ele-lean@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 00:43:52 by ele-lean          #+#    #+#             */
-/*   Updated: 2024/08/28 18:20:01 by ele-lean         ###   ########.fr       */
+/*   Updated: 2024/08/28 21:10:15 by ele-lean         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
+
+int		ft_strlen(char *str);
+char	*ft_putnbr_base(long int nbr, char *base_to, int is_neg);
 
 int	is_base_correct(char *base)
 {
@@ -70,37 +73,52 @@ int	decode_base(char *nbr, char *base, int base_len, int start_idx)
 	return (result);
 }
 
-char	*ft_putnbr_base(long int nbr, char *base_to, int is_neg);
+char	*is_minus_zero(int dec_value, char *base_to)
+{
+	char	*res;
+
+	if (dec_value == 0)
+	{
+		res = (char *)malloc(2 * sizeof(char));
+		if (res == NULL)
+			return (NULL);
+		res[0] = base_to[0];
+		res[1] = '\0';
+		return (res);
+	}
+	return (NULL);
+}
 
 char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 {
-	int	sign;
-	int	base_from_len;
-	int	start_idx;
-	int	dec_value;
+	int		sign;
+	int		sti;
+	int		dec_value;
+	char	*res;
 
+	sign = 1;
 	if (!is_base_correct(base_from) || !is_base_correct(base_to))
 		return (NULL);
-	base_from_len = 0;
-	while (base_from[base_from_len])
-		base_from_len++;
-	start_idx = skip_whitespace(nbr);
-	sign = 1;
-	while (nbr[start_idx] != '\0' && (nbr[start_idx] == '+'
-			|| nbr[start_idx] == '-'))
+	sti = skip_whitespace(nbr);
+	while (nbr[sti] != '\0' && (nbr[sti] == '+' || nbr[sti] == '-'))
 	{
-		if (nbr[start_idx] == '-')
+		if (nbr[sti] == '-')
 			sign = -sign;
-		start_idx++;
+		sti++;
 	}
-	dec_value = decode_base(nbr, base_from, base_from_len, start_idx) * sign;
+	dec_value = decode_base(nbr, base_from, ft_strlen(base_from), sti) * sign;
+	res = is_minus_zero(dec_value, base_to);
+	if (res != NULL)
+		return (res);
 	return (ft_putnbr_base(dec_value, base_to, sign < 0));
 }
 
-void	test_convert_base(char *nbr, char *base_from, char *base_to, char *expected)
+/*void	test_convert_base(char *nbr, char 
+*base_from, char *base_to, char *expected)
 {
 	char *result = ft_convert_base(nbr, base_from, base_to);
-	printf("Conversion de '%s' (base '%s') à base '%s':\n", nbr, base_from, base_to);
+	printf("Conversion de '%s' (base '%s') à base '%s':\n",
+	 nbr, base_from, base_to);
 	printf("Résultat attendu: '%s'\n", expected);
 	if (result == NULL)
 		printf("Résultat obtenu: NULL\n");
@@ -111,17 +129,17 @@ void	test_convert_base(char *nbr, char *base_from, char *base_to, char *expected
 }
 
 int	main() {
-	test_convert_base("42", "0123456789", "01", "101010");
+	test_convert_base("-0", "0123456789", "01", "101010");
 	test_convert_base("-42", "0123456789", "01", "-101010");
 	test_convert_base("101010", "01", "0123456789ABCDEF", "2A");
 	test_convert_base("2A", "0123456789ABCDEF", "0123456789", "42");
 	test_convert_base("42", "0123456789", "01234567", "52");
 	test_convert_base("52", "01234567", "0123456789ABCDEF", "2A");
-	test_convert_base("42", "0", "01", "ERREUR");
-	test_convert_base("42", "0123456789", "0", "ERREUR");
-	test_convert_base("42", "01+-", "01", "ERREUR");
-	test_convert_base("42", "0123456789", "01+-", "ERREUR");
+	test_convert_base("42", "0", "01", "NULL");
+	test_convert_base("42", "0123456789", "0", "NULL");
+	test_convert_base("42", "01+-", "01", "NULL");
+	test_convert_base("42", "0123456789", "01+-", "NULL");
 	test_convert_base("", "0123456789", "01", "0");
 	test_convert_base("-10", "0123456789", "01", "-1010");
 	return (0);
-}
+}*/
---
+++ C07/ex05/ft_split.c
@@ -0,0 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ele-lean <ele-lean@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/28 18:30:40 by ele-lean          #+#    #+#             */
+/*   Updated: 2024/08/28 18:31:55 by ele-lean         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	is_in_char(char *charset, char c)
+{
+	int	i;
+
+	i = 0;
+	while (charset[i])
+	{
+		if (charset[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	get_size(char *str, char *charset)
+{
+	int	i;
+	int	size;
+
+	i = 0;
+	size = 0;
+	while (str[i])
+	{
+		if (is_in_char(charset, str[i]) == 0
+			&& (i == 0 || is_in_char(charset, str[i - 1]) == 1))
+			size++;
+		i++;
+	}
+	return (size);
+}
+
+int	get_size_piece(char *str, char *charset)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && is_in_char(charset, str[i]) == 0)
+		i++;
+	return (i);
+}
+
+char	*copy_piece(char *str, int len)
+{
+	char	*piece;
+	int		j;
+
+	piece = (char *)malloc((len + 1) * sizeof(char));
+	if (!piece)
+		return (NULL);
+	j = 0;
+	while (j < len)
+	{
+		piece[j] = str[j];
+		j++;
+	}
+	piece[j] = '\0';
+	return (piece);
+}
+
+char	**ft_split(char *str, char *charset)
+{
+	char	**tab;
+	int		i;
+	int		len;
+
+	tab = (char **)malloc((get_size(str, charset) + 1) * sizeof(char *));
+	if (!tab)
+		return (NULL);
+	i = 0;
+	while (*str)
+	{
+		while (is_in_char(charset, *str))
+			str++;
+		if (*str)
+		{
+			len = get_size_piece(str, charset);
+			tab[i] = copy_piece(str, len);
+			if (!tab[i])
+				return (NULL);
+			i++;
+			str += len;
+		}
+	}
+	tab[i] = 0;
+	return (tab);
+}
+
+/*int	main(void)
+{
+	char **result = ft_split("Salut, 
+	voici une phrase normale, avec différents, séparateurs.", " ,.");
+
+	if (result)
+	{
+		for (int i = 0; result[i]; i++)
+		{
+			printf("%s\n", result[i]);
+			free(result[i]);
+		}
+		free(result);
+	}
+	return (0);
+}
+*/
